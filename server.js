2453895740e895a4cca6acc62f58252188f55d08
rivermanvr <vrgithub@riversconsulting.com>
2018-01-09 13:24:40 -0500

    server.js setup

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require( 'http' );
+const app = require( 'app' );
+const server = http.createServer(app);
+const chalk = require( 'chalk' );
+
+const port = process.env.PORT || 3000;
+
+server.listen(port, () => {
+  console.log(chalk.yellow(`listening on port ${port}`));
+});
