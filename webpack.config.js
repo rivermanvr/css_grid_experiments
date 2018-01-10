194f931ea62fe04b72cbf646f346ae6029041cc7
rivermanvr <vrgithub@riversconsulting.com>
2018-01-09 23:27:03 -0500

    creating webpack.config

--- webpack.config.js
+++ webpack.config.js
@@ -6,5 +6,21 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'dist'),
     filename: 'bundle.js'
-  }
+  },
+  devtool: 'cheap-module-eval-source-map',
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  module: {
+    loaders: [
+      {
+        test: /jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['env', 'react', 'stage-2']
+        }
+      }
+    ]
+  },
 };
