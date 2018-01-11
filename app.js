f29cd5349baf7a085eee54c3900696b10608dcac
rivermanvr <vrgithub@riversconsulting.com>
2018-01-11 15:38:38 -0500

    setup webpack for this project

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require( 'express' );
 const app = express();
 const path = require( 'path' );
-
 const morgan = require( 'morgan' );
 
+app.use('/public', express.static(path.join(__dirname, 'public')));
+app.use('/dist', express.static(path.join(__dirname, 'dist')));
 app.use(morgan( 'dev' ));
 
 app.get('/', (req, res, next) => {
