2cb7ff4fda9221e885e2be29779c7a4a9738b4de
rivermanvr <vrgithub@riversconsulting.com>
2018-01-31 14:25:30 -0500

    moving to React 16...holding on this project for now.

---
+++ src/components/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+
+export default function App() {
+  return (
+    <div className="container-fluid">
+      <h3>Welcome to My Redux Template</h3>
+    </div>
+  )
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
-console.log('hello');
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {HashRouter as Router} from 'react-router-dom';
+import App from './components/App';
+
+ReactDOM.render(
+  <h1>Hello, world!</h1>,
+  document.getElementById('root')
+);
